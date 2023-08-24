343b182970b744dca8de8b8f99d970da068036c4
HoundSL <86515184+janindu-kulathilaka@users.noreply.github.com>
2023-08-24 15:02:52 +0530

    create the ui for login and sign up

---
+++ client/src/components/UserContext.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+import PropTypes from "prop-types";
+import { createContext, useEffect, useState } from "react";
+
+export const UserContext = createContext({});
+
+export function UserContextProvider({ children }) {
+  const [user, setUser] = useState(null);
+  //const [ready, setReady] = useState(false);
+
+  useEffect(() => {
+    if (!user) {
+      axios.get("/profile").then(({ data }) => {
+        setUser(data);
+        //setReady(true);
+      });
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      {children}
+    </UserContext.Provider>
+  );
+}
+
+UserContextProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,26 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary_50: "#e9ebf0",
+        primary_100: "#bcc2d1",
+        primary_200: "#9ba4ba",
+        primary_300: "#6e7b9b",
+        primary_400: "#516187",
+        primary_500: "#263a69",
+        primary_600: "#233560",
+        primary_700: "#1b294b",
+
+        secondary_50: "#eff9f6",
+        secondary_100: "#ccede4",
+        secondary_200: "#b3e4d7",
+        secondary_300: "#90d7c5",
+        secondary_400: "#7bd0ba",
+        secondary_500: "#5ac4a9",
+        secondary_600: "#52b29a",
+      },
+    },
   },
   plugins: [],
 };
